ab11ecb392519eff15dc664595a172ffc7a6f3f4
ChristopherMwiya <129007388+ChristopherMwiya@users.noreply.github.com>
2026-03-08 20:33:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+// PRODUCT LIST
+const products = [
+
+{
+id:1,
+name:"Mango airpods",
+price:150,
+image:"https://via.placeholder.com/200"
+}
+
+]
+
+// DISPLAY PRODUCTS ON HOMEPAGE
+const productList = document.getElementById("productList")
+
+if(productList){
+
+products.forEach(product=>{
+
+productList.innerHTML += `
+
+<div class="product">
+
+<img src="${product.image}">
+<h3>${product.name}</h3>
+<p>K${product.price}</p>
+
+<button onclick="addToCart(${product.id})">
+Add to Cart
+</button>
+
+</div>
+
+`
+
+})
+
+}
+
+// CART SYSTEM
+let cart = JSON.parse(localStorage.getItem("cart")) || []
+
+function addToCart(id){
+
+let product = products.find(p=>p.id === id)
+
+let existing = cart.find(item => item.id === id)
+
+if(existing){
+
+existing.qty += 1
+
+}else{
+
+cart.push({...product, qty:1})
+
+}
+
+localStorage.setItem("cart", JSON.stringify(cart))
+
+alert("Product added to cart!")
+
+}
